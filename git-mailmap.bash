5432f52538d7cd203aeded52af3de4baf9c00071
Skurhse Rage 🌆🌃🌌 <hello@drruruu.dev>
2021-12-25 22:51:57 -0600

    Update docs

--- git-mailmap.bash
+++ git-mailmap.bash
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-# REQ: Installs a gitmailmap entry to alias dead names and emails. <skr 2021-12-17>
+# REQ: Installs a mailmap entry to alias deadnames and emails. <skr 2021-12-17>
 
 # SEE: https://git-scm.com/docs/gitmailmap <>
 
-# USAGE: ./git-repo.bash REPO_PATH OLD_EMAIL NEW_EMAIL NAME <>
-# .............................................................................
+# USAGE: ./git-mailmap.bash REPO_PATH OLD_EMAIL NEW_EMAIL NAME <>
+
+# ..............................................................................
 set +B -Cefuvxo pipefail
 
 cd "$1"
